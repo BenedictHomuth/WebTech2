465b752174acb0877c33e0b35d58c1e5f355a627
BenedictHomuth <benedict.homuth@gmail.com>
2020-04-16 15:10:32 +0200

    next commit

---
+++ Übung 3/Aufgabe 3.1/ws.js
@@ -0,0 +1,47 @@
+var onLoad = function () {
+    //Magic values
+    var ENTER_KEY = 13;
+    var LINK_TO_SERVER = 'ws://194.94.2.20:8087/chat';
+    
+    
+    var terminal = document.getElementById("terminal");
+
+    var connection = new WebSocket(LINK_TO_SERVER);
+
+    connection.onopen = function () {
+        document.getElementById("status").innerHTML = connection.readyState;
+    };
+
+    connection.onclose = function () {
+        document.getElementById("status").innerHTML = connection.readyState;
+    };
+
+    connection.onmessage = function (msg) {
+        document.getElementById("status").innerHTML = connection.readyState;
+        if (msg.data) {
+            terminal.innerHTML += "Server: " + msg.data + "\n";
+            if (msg.data === "bye") {
+                connection.close();
+            }
+        }
+        terminal.scrollTop = terminal.scrollHeight;
+        document.getElementById("status").innerHTML = connection.readyState;
+    };
+
+    //Check for input (send message)
+    document.getElementById("input").onkeydown = function (event) {
+        if (event.keyCode === ENTER_KEY) {
+            var msg = document.getElementById("input").value;
+            if (msg) {
+                connection.send(msg);
+                document.getElementById("input").value = "";
+                terminal.innerHTML += "You: " + msg + "\n";
+                terminal.scrollTop = terminal.scrollHeight;
+            }
+        }
+        document.getElementById("status").innerHTML = connection.readyState;
+    }
+}
+
+//Start script
+onLoad();
